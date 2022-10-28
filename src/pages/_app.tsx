2326e8505ec2e2a549e35bf9191dab38b107f9c5
Codeaid <codeaid@users.noreply.github.com>
2022-10-28 16:57:31 +0100

    Enabled usage of custom fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'modern-normalize/modern-normalize.css';
+import 'styles/global.css';
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      <link href="https://fonts.googleapis.com" rel="preconnect" />
+      <link href="https://fonts.gstatic.com" rel="preconnect" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=BenchNine&family=Fira+Sans:ital@0;1&display=swap"
+        rel="stylesheet"
+      />
+
+      {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+      <title>Way Of The Hunter - Hit Energy Calculator</title>
+    </Head>
+    <body>
+      <div className="overlay"></div>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
