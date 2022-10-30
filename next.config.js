094cb0a49e27888d564d846547f7b4e53d1f501b
Codeaid <codeaid@users.noreply.github.com>
2022-10-30 01:33:47 +0100

    Disabled Next.js image optimization API

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
