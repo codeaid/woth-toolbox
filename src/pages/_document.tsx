63c042bf0c84bfcbe6d37d95485e5b9085157da6
Codeaid <codeaid@users.noreply.github.com>
2022-10-30 16:07:07 +0000

    Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { baseUrl } from 'config/app';
 
 const Document = () => (
   <Html>
@@ -9,6 +10,7 @@ const Document = () => (
         href="https://fonts.googleapis.com/css2?family=BenchNine&family=Fira+Sans:ital@0;1&display=swap"
         rel="stylesheet"
       />
+      <link rel="shortcut icon" href={`${baseUrl}/img/favicon.png`} />
     </Head>
     <body>
       <div className="overlay"></div>
