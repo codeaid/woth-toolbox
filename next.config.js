41b8a22d6dd6b31b3d079a6a58642c0b843d9c35
Codeaid <codeaid@users.noreply.github.com>
2022-10-30 13:36:56 +0000

    Enabled CSS class name hashing in production

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+const loaderUtils = require('loader-utils');
+const path = require('path');
+
+// @see https://gist.github.com/herlon214/7da6a39b885014a5d5f51716f6530ce4
+const getLocalIdentHash = (context, localIdentName, localName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, '/')}#className:${localName}`,
+      ),
+      'md4',
+      'base64',
+      6,
+    )
+    .replace(/^(-?\d|--)/, '_$1')
+    .replaceAll('+', '_')
+    .replaceAll('/', '_');
+
 /** @type {import('next').NextConfig} */
 const config = {
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
@@ -7,6 +27,28 @@ const config = {
   },
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { dev }) => {
+    const rules = config.module.rules
+      .find(rule => typeof rule.oneOf === 'object')
+      .oneOf.filter(rule => Array.isArray(rule.use));
+
+    // Don't change names in development mode
+    if (dev) {
+      return config;
+    }
+
+    rules.forEach(rule => {
+      rule.use.forEach(moduleLoader => {
+        if (
+          moduleLoader.loader?.includes('css-loader') &&
+          !moduleLoader.loader?.includes('postcss-loader')
+        )
+          moduleLoader.options.modules.getLocalIdent = getLocalIdentHash;
+      });
+    });
+
+    return config;
+  },
 };
 
 module.exports = config;
