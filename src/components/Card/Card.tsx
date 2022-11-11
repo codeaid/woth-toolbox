18ba4be63c338b60e91519e61df3b806a9f112bb
Codeaid <codeaid@users.noreply.github.com>
2022-11-11 16:30:05 +0000

    Added Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from 'react';
+import styles from './Card.module.css';
+
+export const Card = (props: PropsWithChildren) => {
+  const { children } = props;
+
+  return <div className={styles.Card}>{children}</div>;
+};
