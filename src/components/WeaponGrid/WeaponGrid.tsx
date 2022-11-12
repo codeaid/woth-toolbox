2fb2903ada5841fc4afad8ee6d4e1b5667e147d0
Codeaid <codeaid@users.noreply.github.com>
2022-11-12 19:23:47 +0000

    Moved weapon grid header to a separate component

--- src/components/WeaponGrid/WeaponGrid.tsx
+++ src/components/WeaponGrid/WeaponGrid.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { getWeaponGroups } from 'lib/weapons';
 import { WeaponGridGroup } from './WeaponGridGroup';
+import { WeaponGridHeader } from "./WeaponGridHeader";
 import { WeaponGridProps } from './types';
 import styles from './WeaponGrid.module.css';
 
@@ -12,13 +13,7 @@ export const WeaponGrid = (props: WeaponGridProps) => {
 
   return (
     <div className={styles.WeaponGrid}>
-      <div className={styles.WeaponGridHeader}>
-        <div className={styles.WeaponGridHeaderDistance}>50m</div>
-        <div className={styles.WeaponGridHeaderDistance}>100m</div>
-        <div className={styles.WeaponGridHeaderDistance}>150m</div>
-        <div className={styles.WeaponGridHeaderDistance}>200m</div>
-        <div className={styles.WeaponGridHeaderDistance}>300m</div>
-      </div>
+      <WeaponGridHeader />
 
       {weaponGroups.map(group => (
         <WeaponGridGroup animal={animal} group={group} key={group.tier} />
