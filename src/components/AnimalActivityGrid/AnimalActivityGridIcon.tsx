7044b3e960577ac64011b36e6f8ba0542f90edd8
Codeaid <codeaid@users.noreply.github.com>
2022-11-24 01:06:30 +0000

    Updated AnimalActivityGrid to use Marker components

--- src/components/AnimalActivityGrid/AnimalActivityGridIcon.tsx
+++ src/components/AnimalActivityGrid/AnimalActivityGridIcon.tsx
@@ -1,6 +1,5 @@
 import classnames from 'classnames';
-import Image from 'next/image';
-import { baseUrl } from 'config/app';
+import { Marker } from 'components/Marker';
 import { AnimalActivity } from 'types/animals';
 import { AnimalActivityGridIconProps } from './types';
 import styles from './AnimalActivityGridIcon.module.css';
@@ -15,26 +14,17 @@ export const AnimalActivityGridIcon = (props: AnimalActivityGridIconProps) => {
     className: classnames({
       [styles.AnimalActivityGridIconIntermediate]: intermediate,
     }),
-    draggable: false,
-    height: 24,
+    size: 24,
     title,
-    width: 24,
   };
 
   switch (activity) {
     case AnimalActivity.Drinking:
-      return (
-        <Image {...imgProps} alt="Drinking" src={`${baseUrl}/img/drink.png`} />
-      );
+      return <Marker {...imgProps} type="zone:drink" />;
     case AnimalActivity.Feeding:
-      return (
-        <Image {...imgProps} alt="Feeding" src={`${baseUrl}/img/feed.png`} />
-      );
+      return <Marker {...imgProps} type="zone:eat" />;
     case AnimalActivity.Sleeping:
-      return (
-        <Image {...imgProps} alt="Sleeping" src={`${baseUrl}/img/sleep.png`} />
-      );
-
+      return <Marker {...imgProps} type="zone:sleep" />;
     default:
       return null;
   }
