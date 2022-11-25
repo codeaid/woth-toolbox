b7281b4e5edaf167c3d445c34a327b2ca1e24589
Codeaid <codeaid@users.noreply.github.com>
2022-11-25 17:44:46 +0000

    Added Idaho cabin marker positions

--- src/config/idaho.ts
+++ src/config/idaho.ts
@@ -1,6 +1,15 @@
 import { createMarkerPositionConverter } from 'lib/markers';
 import { MarkerPosition } from 'types/markers';
 
+export const cabinMarkers = (
+  [
+    [0.7939453125, 0.242919921875],
+    [0.248046875, 0.3203125],
+    [0.2060546875, 0.685546875],
+    [0.7568359375, 0.8310546875],
+  ] as Array<MarkerPosition>
+).map(createMarkerPositionConverter('cabin'));
+
 export const lodgeMarkers = (
   [[0.66796875, 0.47509765625]] as Array<MarkerPosition>
 ).map(createMarkerPositionConverter('lodge'));
--- src/pages/nez-perce-valley/config.ts
+++ src/pages/nez-perce-valley/config.ts
@@ -1,4 +1,4 @@
-import { lodgeMarkers } from 'config/idaho';
+import { cabinMarkers, lodgeMarkers } from 'config/idaho';
 import { MarkerOptions, MarkerType } from 'types/markers';
 
 // Map size configuration
@@ -6,7 +6,13 @@ export const mapHeight = 4096;
 export const mapWidth = 4096;
 
 // List of generic markers to show on the Nex Perce map
-export const genericMarkers: Array<MarkerOptions> = [lodgeMarkers].flat();
+export const genericMarkers: Array<MarkerOptions> = [
+  cabinMarkers,
+  lodgeMarkers,
+].flat();
 
 // Map of marker types and their maximum map scale visibility
-export const markerVisibilityMap = new Map<MarkerType, number>([['lodge', 0]]);
+export const markerVisibilityMap = new Map<MarkerType, number>([
+  ['cabin', 0.25],
+  ['lodge', 0],
+]);
