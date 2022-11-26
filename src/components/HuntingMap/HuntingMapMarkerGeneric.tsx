132e97861ecea4571a412bc25f07fd3c6118b562
Codeaid <codeaid@users.noreply.github.com>
2022-11-26 16:12:01 +0000

    Added support for marker references to HuntingMapMarkerGeneric

--- src/components/HuntingMap/HuntingMapMarkerGeneric.tsx
+++ src/components/HuntingMap/HuntingMapMarkerGeneric.tsx
@@ -1,81 +1,89 @@
 import classnames from 'classnames';
-import { useCallback, useMemo, useRef } from 'react';
+import { ForwardedRef, forwardRef, useCallback, useMemo, useRef } from 'react';
 import { Transition } from 'react-transition-group';
 import { Marker } from 'components/Marker';
+import { useRefCallback } from 'hooks';
 import { getMarkerSize, isMarkerVisibleAtScale } from 'lib/markers';
 import { HuntingMapMarkerGenericProps } from './types';
 import styles from './HuntingMapMarker.module.css';
 
-export const HuntingMapMarkerGeneric = (
-  props: HuntingMapMarkerGenericProps,
-) => {
-  const {
-    className,
-    mapScale,
-    marker,
-    markerVisibilityMap,
-    maxMarkerSize,
-    visible = true,
-    onClick,
-  } = props;
+export const HuntingMapMarkerGeneric = forwardRef(
+  (
+    props: HuntingMapMarkerGenericProps,
+    ref: ForwardedRef<HTMLImageElement>,
+  ) => {
+    const {
+      className,
+      mapScale,
+      marker,
+      markerVisibilityMap,
+      maxMarkerSize,
+      visible = true,
+      onClick,
+    } = props;
 
-  // Extract marker details
-  const { coords, type } = marker;
-  const [left, top] = coords;
+    // Extract marker details
+    const { coords, type } = marker;
+    const [left, top] = coords;
 
-  // Reference to marker's image element
-  const markerRef = useRef<HTMLImageElement>(null);
+    // Reference to marker's image element
+    const markerRef = useRef<HTMLImageElement>(null);
 
-  // Check if the marker is visible at the current scale
-  const visibleAtScale = useMemo(
-    () => isMarkerVisibleAtScale(mapScale, marker.type, markerVisibilityMap),
-    [mapScale, marker, markerVisibilityMap],
-  );
+    const [, setInnerRef] = useRefCallback<HTMLImageElement>(markerRef, ref);
 
-  // Calculate marker size at the current map scale
-  const size = useMemo(
-    () => getMarkerSize(mapScale, maxMarkerSize),
-    [maxMarkerSize, mapScale],
-  );
+    // Check if the marker is visible at the current scale
+    const visibleAtScale = useMemo(
+      () => isMarkerVisibleAtScale(mapScale, marker.type, markerVisibilityMap),
+      [mapScale, marker, markerVisibilityMap],
+    );
 
-  /**
-   * Handle clicking on the marker
-   */
-  const handleClick = useCallback(
-    () => onClick && onClick(marker),
-    [marker, onClick],
-  );
+    // Calculate marker size at the current map scale
+    const size = useMemo(
+      () => getMarkerSize(mapScale, maxMarkerSize),
+      [maxMarkerSize, mapScale],
+    );
 
-  return (
-    <Transition
-      in={visible && visibleAtScale}
-      mountOnEnter={true}
-      nodeRef={markerRef}
-      timeout={150}
-      unmountOnExit={true}
-    >
-      {state => (
-        <Marker
-          className={classnames(
-            styles.HuntingMapMarker,
-            {
-              [styles.HuntingMapMarkerStateEntering]: state === 'entering',
-              [styles.HuntingMapMarkerStateEntered]: state === 'entered',
-              [styles.HuntingMapMarkerStateExiting]: state === 'exiting',
-              [styles.HuntingMapMarkerStateExited]: state === 'exited',
-            },
-            className,
-          )}
-          ref={markerRef}
-          size={size}
-          style={{
-            left: `calc(${left * 100}% - ${size / 2}px)`,
-            top: `calc(${top * 100}% - ${size / 2}px)`,
-          }}
-          type={type}
-          onClick={handleClick}
-        />
-      )}
-    </Transition>
-  );
-};
+    /**
+     * Handle clicking on the marker
+     */
+    const handleClick = useCallback(
+      () => onClick && onClick(marker),
+      [marker, onClick],
+    );
+
+    return (
+      <Transition
+        in={visible && visibleAtScale}
+        mountOnEnter={true}
+        nodeRef={markerRef}
+        timeout={150}
+        unmountOnExit={true}
+      >
+        {state => (
+          <Marker
+            className={classnames(
+              styles.HuntingMapMarker,
+              {
+                [styles.HuntingMapMarkerStateEntering]: state === 'entering',
+                [styles.HuntingMapMarkerStateEntered]: state === 'entered',
+                [styles.HuntingMapMarkerStateExiting]: state === 'exiting',
+                [styles.HuntingMapMarkerStateExited]: state === 'exited',
+              },
+              className,
+            )}
+            ref={setInnerRef}
+            size={size}
+            style={{
+              left: `calc(${left * 100}% - ${size / 2}px)`,
+              top: `calc(${top * 100}% - ${size / 2}px)`,
+            }}
+            type={type}
+            onClick={handleClick}
+          />
+        )}
+      </Transition>
+    );
+  },
+);
+
+HuntingMapMarkerGeneric.displayName = 'HuntingMapMarkerGeneric';
