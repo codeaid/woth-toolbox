b6395a7ee838dde8b14a8b33e0f0c3e8dd8b088d
Codeaid <codeaid@users.noreply.github.com>
2022-11-27 01:04:37 +0000

    Removed debug code

--- src/pages/nez-perce-valley/index.tsx
+++ src/pages/nez-perce-valley/index.tsx
@@ -3,8 +3,7 @@ import { useState } from 'react';
 import NoSSR from 'react-no-ssr';
 import { HuntingMap } from 'components/HuntingMap';
 import { baseUrl } from 'config/app';
-import { AnimalType } from 'types/animals';
-import { MarkerPosition, MarkerType } from 'types/markers';
+import { MarkerType } from 'types/markers';
 import {
   enabledTypes,
   mapHeight,
@@ -13,39 +12,6 @@ import {
 } from './config';
 import { animalMarkers } from './markers/animals';
 import { genericMarkers } from './markers/generic';
-
-let markers: Array<MarkerPosition> = [];
-
-function consoleWithNoSource(...params: Array<any>) {
-  setTimeout(console.log.bind(console, ...params));
-}
-
-const animal: AnimalType = 'animal:gray wolf';
-
-const callback = (x: number, y: number) => {
-  const percentX = x / mapWidth;
-  const percentY = y / mapHeight;
-
-  markers.push([percentX, percentY]);
-
-  consoleWithNoSource(`[${percentX}, ${percentY}] read (${markers.length})`);
-
-  if (markers.length === 7) {
-    const sleep = markers.splice(-2) as any;
-    const eat = markers.splice(-2) as any;
-    const drink = markers.splice(-2) as any;
-    const coords = markers.pop();
-
-    consoleWithNoSource(`
-createAnimalMarker(
-  '${animal}',
-  ${JSON.stringify(coords)},
-  ${JSON.stringify(drink)},
-  ${JSON.stringify(eat)},
-  ${JSON.stringify(sleep)},
-),`);
-  }
-};
 
 const NezPerceValleyPage = () => {
   // Marker filter state
@@ -67,7 +33,6 @@ const NezPerceValleyPage = () => {
           genericMarkers={genericMarkers}
           markerFilter={markerFilter}
           markerVisibilityMap={markerVisibilityMap}
-          onClick={callback}
           onFilterChange={setMarkerFilter}
         />
       </NoSSR>
