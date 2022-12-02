c161fa95d7a2d33489e3005611998fc4fa6bb4d7
Codeaid <codeaid@users.noreply.github.com>
2022-12-02 01:37:36 +0000

    Added support for zooming map with +, - and / keyboard keys

--- src/components/HuntingMapToolbar/HuntingMapToolbar.tsx
+++ src/components/HuntingMapToolbar/HuntingMapToolbar.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect } from 'react';
 import { RiArrowGoBackFill, RiZoomInLine, RiZoomOutLine } from 'react-icons/ri';
 import { IconButton } from 'components/IconButton';
 import { HuntingMapToolbarProps } from './types';
@@ -5,6 +6,38 @@ import styles from './HuntingMapToolbar.module.css';
 
 export const HuntingMapToolbar = (props: HuntingMapToolbarProps) => {
   const { onReset, onZoomIn, onZoomOut } = props;
+
+  /**
+   * Handle pressing keyboard keys
+   */
+  const handleDocumentKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      switch (event.key) {
+        case '+':
+        case '=':
+          onZoomIn();
+          break;
+        case '-':
+          onZoomOut();
+          break;
+        case '/':
+          onReset();
+          break;
+        default:
+          break;
+      }
+    },
+    [onReset, onZoomIn, onZoomOut],
+  );
+
+  // Monitor keyboard key presses
+  useEffect(() => {
+    document.addEventListener('keypress', handleDocumentKeyPress);
+
+    return () => {
+      document.removeEventListener('keypress', handleDocumentKeyPress);
+    };
+  }, [handleDocumentKeyPress]);
 
   return (
     <div className={styles.HuntingMapToolbar}>
