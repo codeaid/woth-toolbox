4f81f46a6eb2dfd46dc204fc451fffb3b216f973
Codeaid <codeaid@users.noreply.github.com>
2022-12-06 14:41:37 +0000

    Added icon component retrieval helpers

---
+++ src/lib/icons.ts
@@ -0,0 +1,11 @@
+import { DefaultIcon } from 'components/Icon';
+import { iconComponentMap } from 'config/icons';
+import { MarkerType } from 'types/markers';
+
+/**
+ * Get icon component associated with the specified marker type
+ *
+ * @param type Target marker type
+ */
+export const getIconComponent = (type: MarkerType) =>
+  iconComponentMap.get(type) ?? DefaultIcon;
