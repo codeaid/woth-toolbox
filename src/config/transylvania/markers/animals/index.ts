3c7504e42c517c6f21d50fa90eee2a337485f078
Codeaid <codeaid@users.noreply.github.com>
2022-12-07 23:24:04 +0000

    Added brown bear markers to Transylvania map

--- src/config/transylvania/markers/animals/index.ts
+++ src/config/transylvania/markers/animals/index.ts
@@ -1,1 +1,2 @@
+export * from './brownBear';
 export * from './goldenJackal';
--- src/pages/transylvania/markers/animals.ts
+++ src/pages/transylvania/markers/animals.ts
@@ -1,6 +1,7 @@
-import { goldenJackalMarkers } from 'config/transylvania';
+import { brownBearMarkers, goldenJackalMarkers } from 'config/transylvania';
 import { AnimalMarkerOptions } from 'types/markers';
 
 export const animalMarkers: Array<AnimalMarkerOptions> = [
+  brownBearMarkers,
   goldenJackalMarkers,
 ].flat();
