584d33bd298589b9044891cca58607632f7004e7
Codeaid <codeaid@users.noreply.github.com>
2022-12-10 01:36:47 +0000

    Implemented settings panel toolbar button

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -2,8 +2,11 @@ import classnames from 'classnames';
 import { useRouter } from 'next/router';
 import { MouseEvent, useCallback, useEffect, useMemo, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { RiMenuLine } from 'react-icons/ri';
+import { RiMenuLine, RiSettings2Line } from 'react-icons/ri';
+import { IconButton } from 'components/IconButton';
 import { NavLink } from 'components/NavLink';
+import { SettingsEditor } from 'components/SettingsEditor';
+import { useApplicationSettings } from 'hooks';
 import { ToolbarProps } from './types';
 import styles from './Toolbar.module.css';
 
@@ -12,6 +15,12 @@ export const Toolbar = (props: ToolbarProps) => {
 
   // Flag indicating whether the mobile menu is currently visible
   const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  // Flag indicating whether the settings panel is visible
+  const [settingsVisible, setSettingsVisible] = useState(false);
+
+  // Extract current application settings and their updater
+  const { settings, onSettingsChange } = useApplicationSettings();
 
   // Retrieve current application path
   const { asPath } = useRouter();
@@ -32,6 +41,11 @@ export const Toolbar = (props: ToolbarProps) => {
 
   // Mobile menu contents
   const mobileMenu = useMemo(() => {
+    // Ensure browser is initialized
+    if (typeof window === 'undefined') {
+      return null;
+    }
+
     // Ensure menu is visible before proceeding
     if (!mobileMenuVisible) {
       return;
@@ -40,9 +54,22 @@ export const Toolbar = (props: ToolbarProps) => {
     // Render menu into the layout content component
     return createPortal(
       <div className={styles.ToolbarMobileMenu}>{children}</div>,
-      document.getElementById('layout-content') || document.body,
+      document.getElementById('layout-content') ?? document.body,
     );
   }, [children, mobileMenuVisible]);
+
+  /**
+   * Handle hiding settings
+   */
+  const handleHideSettings = useCallback(() => setSettingsVisible(false), []);
+
+  /**
+   * Handle toggling settings
+   */
+  const handleToggleSettings = useCallback(
+    () => setSettingsVisible(current => !current),
+    [],
+  );
 
   /**
    * Handle showing or hiding mobile menu
@@ -56,6 +83,24 @@ export const Toolbar = (props: ToolbarProps) => {
     },
     [],
   );
+
+  // Rendered settings panel
+  const renderedSettings = useMemo(() => {
+    // Ensure browser is initialized
+    if (typeof window === 'undefined') {
+      return null;
+    }
+
+    return createPortal(
+      <SettingsEditor
+        settings={settings}
+        visible={settingsVisible}
+        onChange={onSettingsChange}
+        onClose={handleHideSettings}
+      />,
+      document.getElementById('layout-content') ?? document.body,
+    );
+  }, [handleHideSettings, onSettingsChange, settings, settingsVisible]);
 
   // Hide mobile menu when navigating to a new page
   useEffect(() => setMobileMenuVisible(false), [asPath]);
@@ -79,7 +124,13 @@ export const Toolbar = (props: ToolbarProps) => {
           <div className={styles.ToolbarSubtitle}>{subtitle}</div>
         </div>
         <div className={styles.ToolbarActions}>{children}</div>
+        <div className={styles.ToolbarSettingsButton}>
+          <IconButton onClick={handleToggleSettings}>
+            <RiSettings2Line />
+          </IconButton>
+        </div>
       </div>
+      {renderedSettings}
       {mobileMenu}
     </>
   );
