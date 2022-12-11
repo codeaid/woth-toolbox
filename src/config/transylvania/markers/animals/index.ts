291e507dd9b7beb44bcabb6302763c4508072d65
Codeaid <codeaid@users.noreply.github.com>
2022-12-11 14:45:42 +0000

    Added greylag geese markers to Transylvania map

--- src/config/transylvania/markers/animals/index.ts
+++ src/config/transylvania/markers/animals/index.ts
@@ -2,6 +2,7 @@ export * from './brownBear';
 export * from './chamois';
 export * from './europeanHare';
 export * from './goldenJackal';
+export * from './greylagGoose';
 export * from './redDeer';
 export * from './redFox';
 export * from './wildDuck';
