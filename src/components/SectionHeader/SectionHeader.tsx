83d66fd457fa9716007795e24327efdbbdb08bfb
Codeaid <codeaid@users.noreply.github.com>
2022-12-11 01:26:32 +0000

    Exposed native SectionHeader HTML properties

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -1,11 +1,12 @@
-import { PropsWithChildren } from 'react';
+import classnames from 'classnames';
 import styles from './SectionHeader.module.css';
+import { SectionHeaderProps } from './types';
 
-export const SectionHeader = (props: PropsWithChildren) => {
-  const { children } = props;
+export const SectionHeader = (props: SectionHeaderProps) => {
+  const { children, className, ...rest } = props;
 
   return (
-    <div className={styles.SectionHeader}>
+    <div className={classnames(styles.SectionHeader, className)} {...rest}>
       <div className={styles.SectionHeaderContent}>{children}</div>
     </div>
   );
---
+++ src/components/SectionHeader/types.ts
@@ -0,0 +1,6 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export type SectionHeaderProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
