b9428d585fcd65fb2f45a41ea5277a2facaaa820
Codeaid <codeaid@users.noreply.github.com>
2022-12-14 23:43:52 +0000

    Added Error component

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,12 @@
+import { ErrorProps } from './types';
+import styles from './Error.module.css';
+
+export const Error = (props: ErrorProps) => {
+  const { status } = props;
+
+  return (
+    <div className={styles.Error}>
+      <h1 className={styles.ErrorHeading}>{status}</h1>
+    </div>
+  );
+};
