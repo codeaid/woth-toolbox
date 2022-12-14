4f96b84a002f652f8eb25d78eb7f2482e38d38d3
Codeaid <codeaid@users.noreply.github.com>
2022-12-14 21:41:30 +0000

    Added zero width space HTML constants

---
+++ src/config/html.ts
@@ -0,0 +1,5 @@
+// @see https://en.wikipedia.org/wiki/Zero-width_space
+export const zeroWidthSpace = '\u200b';
+
+// @see https://en.wikipedia.org/wiki/Zero-width_non-joiner
+export const zeroWidthNonJoiner = '\u200c';
