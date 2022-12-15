13aaf889c4e050380a4f56ce7dd3b2621da5c203
Codeaid <codeaid@users.noreply.github.com>
2022-12-15 18:03:28 +0000

    Added mouflon markers to Transylvania map

--- src/config/transylvania/markers/animals/index.ts
+++ src/config/transylvania/markers/animals/index.ts
@@ -6,6 +6,7 @@ export * from './fallowDeer';
 export * from './goldenJackal';
 export * from './grayWolf';
 export * from './greylagGoose';
+export * from './mouflon';
 export * from './pheasant';
 export * from './redDeer';
 export * from './redFox';
