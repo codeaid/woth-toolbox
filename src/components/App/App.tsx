2b5af612ee2b7814cf7f8183f7e876bb8026a0ae
Codeaid <codeaid@users.noreply.github.com>
2022-12-16 22:52:49 +0000

    Removed dependency on external locale from useLocaleResource

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,16 @@
 import { AppProps } from 'next/app';
 import { IntlProvider } from 'react-intl';
 import { Layout } from 'components/Layout';
-import { useLocale, useLocaleResource, useSettings } from 'hooks';
+import { useLocale, useLocaleResource } from 'hooks';
 
 export const App = (props: AppProps) => {
   const { Component, pageProps } = props;
 
   // Retrieve current application settings and locale
-  const { initialized, settings } = useSettings();
   const locale = useLocale();
 
   // Retrieve locale's translation messages
-  const messages = useLocaleResource(initialized ? settings.locale : undefined);
+  const messages = useLocaleResource();
 
   return (
     <IntlProvider locale={locale} messages={messages}>
--- src/hooks/useLocaleResource.ts
+++ src/hooks/useLocaleResource.ts
@@ -1,25 +1,27 @@
 import { useEffect, useState } from 'react';
+import { useSettings } from 'hooks';
 import { getLocaleMessagesAsync } from 'lib/i18n';
 import { TranslationResource } from 'types/i18n';
 
 /**
  * Retrieve message resources associated with the specified locale
- *
- * @param locale Target locale
  */
-export const useLocaleResource = (locale?: string) => {
+export const useLocaleResource = () => {
+  // Retrieve current application settings
+  const { initialized, settings } = useSettings();
+
   // Locale and its resource messages
   const [messages, setMessages] = useState<TranslationResource>();
 
   // Load new message data on initial load and locale change
   useEffect(() => {
     // Ensure locale is present before fetching resources
-    if (!locale) {
+    if (!initialized || !settings.locale) {
       return;
     }
 
-    getLocaleMessagesAsync(locale).then(setMessages);
-  }, [locale]);
+    getLocaleMessagesAsync(settings.locale).then(setMessages);
+  }, [initialized, settings.locale]);
 
   return messages;
 };
