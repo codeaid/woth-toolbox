d7d3a94677b6598bd6e60741f117f21db4b87182
Codeaid <codeaid@users.noreply.github.com>
2022-12-20 15:57:17 +0000

    Added Heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,67 @@
+import classnames from 'classnames';
+import { useMemo } from 'react';
+import { HeadingProps } from './types';
+import styles from './Heading.module.css';
+
+export const Heading = (props: HeadingProps) => {
+  const { children, className, size, ...rest } = props;
+
+  // Generate component class names
+  const classNames = useMemo(
+    () =>
+      classnames(
+        styles.Heading,
+        {
+          [styles.Heading1]: size === 1,
+          [styles.Heading2]: size === 2,
+          [styles.Heading3]: size === 3,
+          [styles.Heading4]: size === 4,
+          [styles.Heading5]: size === 5,
+          [styles.Heading6]: size === 6,
+        },
+        className,
+      ),
+    [className, size],
+  );
+
+  return useMemo(() => {
+    switch (size) {
+      case 1:
+        return (
+          <h1 {...rest} className={classNames}>
+            {children}
+          </h1>
+        );
+      case 2:
+        return (
+          <h2 {...rest} className={classNames}>
+            {children}
+          </h2>
+        );
+      case 3:
+        return (
+          <h3 {...rest} className={classNames}>
+            {children}
+          </h3>
+        );
+      case 4:
+        return (
+          <h4 {...rest} className={classNames}>
+            {children}
+          </h4>
+        );
+      case 5:
+        return (
+          <h5 {...rest} className={classNames}>
+            {children}
+          </h5>
+        );
+      case 6:
+        return (
+          <h6 {...rest} className={classNames}>
+            {children}
+          </h6>
+        );
+    }
+  }, [children, classNames, rest, size]);
+};
---
+++ src/components/Heading/types.ts
@@ -0,0 +1,5 @@
+import { HTMLAttributes } from 'react';
+
+export type HeadingProps = HTMLAttributes<HTMLHeadingElement> & {
+  size: 1 | 2 | 3 | 4 | 5 | 6;
+};
