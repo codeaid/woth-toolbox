6530498ba9a562e94cf54a15a46fcec52bf5e194
Codeaid <codeaid@users.noreply.github.com>
2022-12-20 16:08:41 +0000

    Removed unused marker option snippet generation function

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -12,37 +12,6 @@ export const consoleLogClean = (...params: Array<any>) =>
   setTimeout(console.log.bind(console, ...params));
 
 /**
- * Generate marker option registration code snippet
- *
- * @param animalType Target animal type
- * @param positions List of marker positions to process
- * @param drinkZoneCount Number of drink zones to print
- * @param eatZoneCount Number of feed zones to print
- * @param sleepZoneCount Number of sleep zones to print
- */
-export const createMarkerOptionSnippet = (
-  animalType: AnimalType,
-  positions: Array<Point>,
-  drinkZoneCount: number,
-  eatZoneCount: number,
-  sleepZoneCount: number,
-) => {
-  // Split the list of coordinates into zone chunks
-  const [coords] = positions.splice(0, 1);
-  const drink = positions.splice(0, drinkZoneCount);
-  const eat = positions.splice(0, eatZoneCount);
-  const sleep = positions.splice(0, sleepZoneCount);
-
-  return `createAnimalMarkerOptions(
-  '${animalType}',
-  ${JSON.stringify(coords)},
-  ${JSON.stringify(drink)},
-  ${JSON.stringify(eat)},
-  ${JSON.stringify(sleep)},
-),`;
-};
-
-/**
  * Copy text to clipboard
  *
  * @param text Text to copy
