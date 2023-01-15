269e8ee7fff6bda4d6fd1b66b5c9aa4acad70c18
Codeaid <codeaid@users.noreply.github.com>
2023-01-15 22:36:18 +0000

    Added Emphasis component

---
+++ src/components/Emphasis/Emphasis.tsx
@@ -0,0 +1,6 @@
+import { EmphasisProps } from './types';
+import styles from './Emphasis.module.css';
+
+export const Emphasis = (props: EmphasisProps) => (
+  <span {...props} className={styles.Emphasis} />
+);
---
+++ src/components/Emphasis/types.ts
@@ -0,0 +1,6 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export type EmphasisProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLSpanElement>,
+  HTMLSpanElement
+>;
