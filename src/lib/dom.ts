8ad954756bd43e6ae62a20ddf8ab495b09722f1f
Codeaid <codeaid@users.noreply.github.com>
2023-01-16 00:59:40 +0000

    Added getTransitionClassName helper function

--- src/lib/dom.ts
+++ src/lib/dom.ts
@@ -1,4 +1,37 @@
+import classnames from 'classnames';
 import type { Reference } from 'types/dom';
+import { TransitionState } from 'types/dom';
+
+/**
+ * Generate CSS classname for the specified transition state
+ *
+ * @param state Source transition state
+ * @param classEntering CSS class name for entering state
+ * @param classEntered CSS class name for entered state
+ * @param classExiting CSS class name for exiting state
+ * @param classExited CSS class name for exited state
+ * @param prepend List of class names to prepend
+ * @param append List of class names to append
+ */
+export const getTransitionClassName = (
+  state: TransitionState,
+  classEntering: string,
+  classEntered: string,
+  classExiting: string,
+  classExited: string,
+  prepend: Array<string> = [],
+  append: Array<string> = [],
+) =>
+  classnames(
+    ...prepend,
+    {
+      [classEntering]: state === 'entering',
+      [classEntered]: state === 'entered',
+      [classExiting]: state === 'exiting',
+      [classExited]: state === 'exited',
+    },
+    ...append,
+  );
 
 /**
  * Update value of the specified reference instance
--- src/types/dom.ts
+++ src/types/dom.ts
@@ -1,6 +1,26 @@
 import { MutableRefObject, RefCallback } from 'react';
+import {
+  ENTERED,
+  ENTERING,
+  EXITED,
+  EXITING,
+  UNMOUNTED,
+} from 'react-transition-group/Transition';
 
 // Reference type combining Ref<T> types of objects as well as reference callbacks
 export type Reference<TValue> = Maybe<
   MutableRefObject<Nullable<TValue>> | RefCallback<TValue>
 >;
+
+export type TransitionEntering = typeof ENTERING;
+export type TransitionEntered = typeof ENTERED;
+export type TransitionExiting = typeof EXITING;
+export type TransitionExited = typeof EXITED;
+export type TransitionUnmounted = typeof UNMOUNTED;
+
+export type TransitionState =
+  | TransitionEntering
+  | TransitionEntered
+  | TransitionExiting
+  | TransitionExited
+  | TransitionUnmounted;
