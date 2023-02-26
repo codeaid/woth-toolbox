2c52429abcf5db0c853f504d8651cceb9c188a21
Codeaid <codeaid@users.noreply.github.com>
2023-02-26 00:17:02 +0000

    Added Alaska map labels

--- src/pages/alaska/config.ts
+++ src/pages/alaska/config.ts
@@ -5,4 +5,55 @@ export const mapHeight = 4096;
 export const mapWidth = 4096;
 
 // List of label to show on the map
-export const mapLabels: Array<MapLabelOptions> = [];
+export const mapLabels: Array<MapLabelOptions> = [
+  {
+    coords: [0.1590300814, 0.2806586437],
+    habitat: 'POI:IDAHO_HABITAT02',
+    name: 'POI:ALASKA_AREA02',
+  },
+  {
+    coords: [0.4072695161, 0.4601719154],
+    habitat: 'POI:TRANSYLVANIA_HABITAT_02',
+    name: 'POI:ALASKA_AREA03',
+  },
+  {
+    coords: [0.4947571286, 0.7116990633],
+    habitat: 'POI:TRANSYLVANIA_HABITAT_02',
+    name: 'POI:ALASKA_AREA05',
+  },
+  {
+    coords: [0.2521944231, 0.4991775651],
+    habitat: 'POI:TRANSYLVANIA_HABITAT_04',
+    name: 'POI:ALASKA_AREA06',
+  },
+  {
+    coords: [0.807761717, 0.6046955831],
+    habitat: 'POI:TRANSYLVANIA_HABITAT_04',
+    name: 'POI:ALASKA_AREA07',
+  },
+  {
+    coords: [0.7209092557, 0.3855813763],
+    habitat: 'POI:TRANSYLVANIA_HABITAT_04',
+    name: 'POI:ALASKA_AREA09',
+  },
+  {
+    coords: [0.1758862567, 0.819577453],
+    habitat: 'POI:TRANSYLVANIA_HABITAT_04',
+    name: 'POI:ALASKA_AREA10',
+  },
+  {
+    coords: [0.851825603, 0.1108114002],
+    habitat: 'POI:TRANSYLVANIA_HABITAT_05',
+    name: 'POI:ALASKA_AREA11',
+  },
+  {
+    coords: [0.2526111941, 0.159434934],
+    habitat: 'POI:IDAHO_HABITAT05',
+    name: 'POI:ALASKA_AREA13',
+  },
+  {
+    coords: [0.7480815905, 0.8676991765],
+    habitat: 'POI:ALASKA_HABITAT_01',
+    name: 'POI:ALASKA_AREA16',
+  },
+];
