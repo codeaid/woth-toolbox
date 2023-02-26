9fb335f1186e00d15997cd03babf7f471ca86e66
Codeaid <codeaid@users.noreply.github.com>
2023-02-26 02:34:25 +0000

    Sorted map label entries

--- src/config/idaho/labels.ts
+++ src/config/idaho/labels.ts
@@ -3,14 +3,39 @@ import { MapLabelOptions } from 'types/cartography';
 // List of label to show on the map
 export const mapLabels: Array<MapLabelOptions> = [
   {
+    coords: [0.135009765625, 0.384033203125],
+    habitat: 'POI:IDAHO_HABITAT05',
+    name: 'POI:IDAHO_AREA01',
+  },
+  {
+    coords: [0.15478515625, 0.57080078125],
+    habitat: 'POI:IDAHO_HABITAT05',
+    name: 'POI:IDAHO_AREA02',
+  },
+  {
     coords: [0.2587890625, 0.191162109375],
     habitat: 'POI:IDAHO_HABITAT05',
     name: 'POI:IDAHO_AREA03',
   },
   {
+    coords: [0.345947265625, 0.830810546875],
+    habitat: 'POI:IDAHO_HABITAT04',
+    name: 'POI:IDAHO_AREA04',
+  },
+  {
+    coords: [0.40380859375, 0.52392578125],
+    habitat: 'POI:IDAHO_HABITAT04',
+    name: 'POI:IDAHO_AREA05',
+  },
+  {
     coords: [0.46875, 0.213134765625],
     habitat: 'POI:IDAHO_HABITAT04',
     name: 'POI:IDAHO_AREA06',
+  },
+  {
+    coords: [0.162958984375, 0.85498046875],
+    habitat: 'POI:IDAHO_HABITAT04',
+    name: 'POI:IDAHO_AREA07',
   },
   {
     coords: [0.68310546875, 0.225341796875],
@@ -28,29 +53,9 @@ export const mapLabels: Array<MapLabelOptions> = [
     name: 'POI:IDAHO_AREA10',
   },
   {
-    coords: [0.135009765625, 0.384033203125],
-    habitat: 'POI:IDAHO_HABITAT05',
-    name: 'POI:IDAHO_AREA01',
-  },
-  {
-    coords: [0.40380859375, 0.52392578125],
-    habitat: 'POI:IDAHO_HABITAT04',
-    name: 'POI:IDAHO_AREA05',
-  },
-  {
     coords: [0.60888671875, 0.51611328125],
     habitat: 'POI:IDAHO_HABITAT02',
     name: 'POI:IDAHO_AREA11',
-  },
-  {
-    coords: [0.783447265625, 0.52392578125],
-    habitat: 'POI:IDAHO_HABITAT03',
-    name: 'POI:IDAHO_AREA13',
-  },
-  {
-    coords: [0.15478515625, 0.57080078125],
-    habitat: 'POI:IDAHO_HABITAT05',
-    name: 'POI:IDAHO_AREA02',
   },
   {
     coords: [0.51025390625, 0.73291015625],
@@ -58,19 +63,14 @@ export const mapLabels: Array<MapLabelOptions> = [
     name: 'POI:IDAHO_AREA12',
   },
   {
+    coords: [0.783447265625, 0.52392578125],
+    habitat: 'POI:IDAHO_HABITAT03',
+    name: 'POI:IDAHO_AREA13',
+  },
+  {
     coords: [0.7802734375, 0.71826171875],
     habitat: 'POI:IDAHO_HABITAT03',
     name: 'POI:IDAHO_AREA14',
-  },
-  {
-    coords: [0.162958984375, 0.85498046875],
-    habitat: 'POI:IDAHO_HABITAT04',
-    name: 'POI:IDAHO_AREA07',
-  },
-  {
-    coords: [0.345947265625, 0.830810546875],
-    habitat: 'POI:IDAHO_HABITAT04',
-    name: 'POI:IDAHO_AREA04',
   },
   {
     coords: [0.541015625, 0.89453125],
