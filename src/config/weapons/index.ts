b2766e88bbb2cf37f9905b7f5e3dd0c28b52951c
Codeaid <codeaid@users.noreply.github.com>
2023-09-07 13:06:37 +0100

    Fixed tier two weapons not being included in the list

--- src/config/weapons/index.ts
+++ src/config/weapons/index.ts
@@ -1,8 +1,15 @@
 import { Weapon } from 'types/weapons';
 import tier1 from './tier1';
+import tier2 from './tier2';
 import tier3 from './tier3';
 import tier4 from './tier4';
 import tier5 from './tier5';
 import tier6 from './tier6';
 
-export const weapons: Array<Weapon> = tier1.concat(tier3, tier4, tier5, tier6);
+export const weapons: Array<Weapon> = tier1.concat(
+  tier2,
+  tier3,
+  tier4,
+  tier5,
+  tier6,
+);
