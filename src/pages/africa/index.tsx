737e3b961f44f91811758a7e41336ce0382baa98
Codeaid <codeaid@users.noreply.github.com>
2024-02-11 13:51:12 +0000

    Replaced map page contents with HuntingMapPage component usage

--- src/pages/africa/index.tsx
+++ src/pages/africa/index.tsx
@@ -1,70 +1,15 @@
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { createPortal } from 'react-dom';
-import { HuntingMap } from 'components/HuntingMap';
-import {
-  animalMarkers,
-  genericMarkers,
-  mapHeight,
-  mapLabels,
-  mapWidth,
-} from 'config/africa';
-import { basePath } from 'config/app';
-import { markerVisibilityMap } from 'config/markers';
-import {
-  useHuntingMapType,
-  useSettings,
-  useTranslator,
-  useTutorial,
-} from 'hooks';
+import { HuntingMapPage } from 'components/HuntingMapPage';
+import { animalMarkers, genericMarkers, mapLabels } from 'config/africa';
 
-const AfricaPage = () => {
-  // Retrieve map type switcher
-  const { onSetMapType } = useHuntingMapType();
-
-  // Render map tutorial dialog
-  const { component: tutorial } = useTutorial(true);
-
-  // Retrieve application settings
-  const { settings } = useSettings();
-
-  // Retrieve application translator
-  const translate = useTranslator();
-
-  // Toggle currently active map type on page load and unload
-  useEffect(() => {
-    onSetMapType('africa');
-    return () => onSetMapType();
-  }, [onSetMapType]);
-
-  return (
-    <>
-      <Head>
-        <title>
-          {`${translate('POI:MAP_NAME_AFRICA')} - ${translate(
-            'UI:GAME_TITLE',
-          )}`}
-        </title>
-      </Head>
-
-      <HuntingMap
-        animalMarkers={animalMarkers}
-        imageHeight={mapHeight}
-        imageScale={2}
-        imageSrc={`${basePath}/img/maps/africa.jpeg`}
-        imageWidth={mapWidth}
-        genericMarkers={genericMarkers}
-        labels={mapLabels}
-        markerSizeAnimal={settings.animalMarkerSize}
-        markerSizeGeneric={settings.genericMarkerSize}
-        markerSizeZone={settings.zoneMarkerSize}
-        markerTrophyRating={settings.animalMarkerRatings}
-        zoomMarkerMap={markerVisibilityMap}
-      />
-
-      {createPortal(tutorial, document.body)}
-    </>
-  );
-};
+const AfricaPage = () => (
+  <HuntingMapPage
+    animalMarkers={animalMarkers}
+    genericMarkers={genericMarkers}
+    mapType="africa"
+    mapImageSrc="/img/maps/africa.jpeg"
+    mapLabels={mapLabels}
+    titleKey="POI:MAP_NAME_AFRICA"
+  />
+);
 
 export default AfricaPage;
