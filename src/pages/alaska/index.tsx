4d319ae23b5170482bed32baf785452e363915b0
Codeaid <codeaid@users.noreply.github.com>
2024-02-13 12:40:49 +0000

    Revert "Replaced map page contents with HuntingMapPage component usage"
    This reverts commit 737e3b961f44f91811758a7e41336ce0382baa98.

--- src/pages/alaska/index.tsx
+++ src/pages/alaska/index.tsx
@@ -1,15 +1,70 @@
-import { HuntingMapPage } from 'components/HuntingMapPage';
-import { animalMarkers, genericMarkers, mapLabels } from 'config/alaska';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { HuntingMap } from 'components/HuntingMap';
+import {
+  animalMarkers,
+  genericMarkers,
+  mapHeight,
+  mapLabels,
+  mapWidth,
+} from 'config/alaska';
+import { basePath } from 'config/app';
+import { markerVisibilityMap } from 'config/markers';
+import {
+  useHuntingMapType,
+  useSettings,
+  useTranslator,
+  useTutorial,
+} from 'hooks';
 
-const AlaskaPage = () => (
-  <HuntingMapPage
-    animalMarkers={animalMarkers}
-    genericMarkers={genericMarkers}
-    mapType="africa"
-    mapImageSrc="/img/maps/alaska.jpeg"
-    mapLabels={mapLabels}
-    titleKey="POI:MAP_NAME_ALASKA"
-  />
-);
+const AlaskaPage = () => {
+  // Retrieve map type switcher
+  const { onSetMapType } = useHuntingMapType();
+
+  // Render map tutorial dialog
+  const { component: tutorial } = useTutorial(true);
+
+  // Retrieve application settings
+  const { settings } = useSettings();
+
+  // Retrieve application translator
+  const translate = useTranslator();
+
+  // Toggle currently active map type on page load and unload
+  useEffect(() => {
+    onSetMapType('alaska');
+    return () => onSetMapType();
+  }, [onSetMapType]);
+
+  return (
+    <>
+      <Head>
+        <title>
+          {`${translate('POI:MAP_NAME_ALASKA')} - ${translate(
+            'UI:GAME_TITLE',
+          )}`}
+        </title>
+      </Head>
+
+      <HuntingMap
+        animalMarkers={animalMarkers}
+        imageHeight={mapHeight}
+        imageScale={2}
+        imageSrc={`${basePath}/img/maps/alaska.jpeg`}
+        imageWidth={mapWidth}
+        genericMarkers={genericMarkers}
+        labels={mapLabels}
+        markerSizeAnimal={settings.animalMarkerSize}
+        markerSizeGeneric={settings.genericMarkerSize}
+        markerSizeZone={settings.zoneMarkerSize}
+        markerTrophyRating={settings.animalMarkerRatings}
+        zoomMarkerMap={markerVisibilityMap}
+      />
+
+      {createPortal(tutorial, document.body)}
+    </>
+  );
+};
 
 export default AlaskaPage;
--- src/pages/nez-perce-valley/index.tsx
+++ src/pages/nez-perce-valley/index.tsx
@@ -1,15 +1,67 @@
-import { HuntingMapPage } from 'components/HuntingMapPage';
-import { animalMarkers, genericMarkers, mapLabels } from 'config/idaho';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { HuntingMap } from 'components/HuntingMap';
+import { basePath } from 'config/app';
+import {
+  animalMarkers,
+  genericMarkers,
+  mapHeight,
+  mapLabels,
+  mapWidth,
+} from 'config/idaho';
+import { markerVisibilityMap } from 'config/markers';
+import {
+  useHuntingMapType,
+  useSettings,
+  useTranslator,
+  useTutorial,
+} from 'hooks';
 
-const NezPerceValleyPage = () => (
-  <HuntingMapPage
-    animalMarkers={animalMarkers}
-    genericMarkers={genericMarkers}
-    mapType="africa"
-    mapImageSrc="/img/maps/nez_perce.jpeg"
-    mapLabels={mapLabels}
-    titleKey="POI:MAP_NAME_IDAHO"
-  />
-);
+const NezPerceValleyPage = () => {
+  // Retrieve map type switcher
+  const { onSetMapType } = useHuntingMapType();
+
+  // Render map tutorial dialog
+  const { component: tutorial } = useTutorial(true);
+
+  // Retrieve application settings
+  const { settings } = useSettings();
+
+  // Retrieve application translator
+  const translate = useTranslator();
+
+  // Toggle currently active map type on page load and unload
+  useEffect(() => {
+    onSetMapType('idaho');
+    return () => onSetMapType();
+  }, [onSetMapType]);
+
+  return (
+    <>
+      <Head>
+        <title>
+          {`${translate('POI:MAP_NAME_IDAHO')} - ${translate('UI:GAME_TITLE')}`}
+        </title>
+      </Head>
+
+      <HuntingMap
+        animalMarkers={animalMarkers}
+        imageHeight={mapHeight}
+        imageSrc={`${basePath}/img/maps/nez_perce.jpeg`}
+        imageWidth={mapWidth}
+        genericMarkers={genericMarkers}
+        labels={mapLabels}
+        markerSizeAnimal={settings.animalMarkerSize}
+        markerSizeGeneric={settings.genericMarkerSize}
+        markerSizeZone={settings.zoneMarkerSize}
+        markerTrophyRating={settings.animalMarkerRatings}
+        zoomMarkerMap={markerVisibilityMap}
+      />
+
+      {createPortal(tutorial, document.body)}
+    </>
+  );
+};
 
 export default NezPerceValleyPage;
