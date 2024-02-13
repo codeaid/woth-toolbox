b8494fa8fed6197e88ecf7a68b41f1f1c9ba6386
Codeaid <codeaid@users.noreply.github.com>
2024-02-13 13:00:34 +0000

    Fixed reverted changes not specifying the correct map types

--- src/pages/new-zealand/index.tsx
+++ src/pages/new-zealand/index.tsx
@@ -1,61 +1,15 @@
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { createPortal } from 'react-dom';
-import { HuntingMap } from 'components/HuntingMap';
-import { basePath } from 'config/app';
-import { markerVisibilityMap } from 'config/markers';
+import { HuntingMapPage } from 'components/HuntingMapPage';
 import { animalMarkers, genericMarkers, mapLabels } from 'config/new-zealand';
-import {
-  useHuntingMapType,
-  useSettings,
-  useTranslator,
-  useTutorial,
-} from 'hooks';
 
-const NewZealandPage = () => {
-  // Retrieve map type switcher
-  const { onSetMapType } = useHuntingMapType();
-
-  // Render map tutorial dialog
-  const { component: tutorial } = useTutorial(true);
-
-  // Retrieve application settings
-  const { settings } = useSettings();
-
-  // Retrieve application translator
-  const translate = useTranslator();
-
-  // Toggle currently active map type on page load and unload
-  useEffect(() => {
-    onSetMapType('new-zealand');
-    return () => onSetMapType();
-  }, [onSetMapType]);
-
-  return (
-    <>
-      <Head>
-        <title>
-          {`${translate('POI:MAP_NAME_NEW_ZEALAND')} - ${translate(
-            'UI:GAME_TITLE',
-          )}`}
-        </title>
-      </Head>
-
-      <HuntingMap
-        animalMarkers={animalMarkers}
-        imageSrc={`${basePath}/img/maps/new_zealand.jpeg`}
-        genericMarkers={genericMarkers}
-        labels={mapLabels}
-        markerSizeAnimal={settings.animalMarkerSize}
-        markerSizeGeneric={settings.genericMarkerSize}
-        markerSizeZone={settings.zoneMarkerSize}
-        markerTrophyRating={settings.animalMarkerRatings}
-        zoomMarkerMap={markerVisibilityMap}
-      />
-
-      {createPortal(tutorial, document.body)}
-    </>
-  );
-};
+const NewZealandPage = () => (
+  <HuntingMapPage
+    animalMarkers={animalMarkers}
+    genericMarkers={genericMarkers}
+    mapType="new-zealand"
+    mapImageSrc="/img/maps/new_zealand.jpeg"
+    mapLabels={mapLabels}
+    titleKey="POI:MAP_NAME_NEW_ZEALAND"
+  />
+);
 
 export default NewZealandPage;
--- src/pages/transylvania/index.tsx
+++ src/pages/transylvania/index.tsx
@@ -1,61 +1,15 @@
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { createPortal } from 'react-dom';
-import { HuntingMap } from 'components/HuntingMap';
-import { basePath } from 'config/app';
-import { markerVisibilityMap } from 'config/markers';
+import { HuntingMapPage } from 'components/HuntingMapPage';
 import { animalMarkers, genericMarkers, mapLabels } from 'config/transylvania';
-import {
-  useHuntingMapType,
-  useSettings,
-  useTranslator,
-  useTutorial,
-} from 'hooks';
 
-const TransylvaniaPage = () => {
-  // Retrieve map type switcher
-  const { onSetMapType } = useHuntingMapType();
-
-  // Render map tutorial dialog
-  const { component: tutorial } = useTutorial(true);
-
-  // Retrieve application settings
-  const { settings } = useSettings();
-
-  // Retrieve application translator
-  const translate = useTranslator();
-
-  // Toggle currently active map type on page load and unload
-  useEffect(() => {
-    onSetMapType('transylvania');
-    return () => onSetMapType();
-  }, [onSetMapType]);
-
-  return (
-    <>
-      <Head>
-        <title>
-          {`${translate('POI:MAP_NAME_TRANSYLVANIA')} - ${translate(
-            'UI:GAME_TITLE',
-          )}`}
-        </title>
-      </Head>
-
-      <HuntingMap
-        animalMarkers={animalMarkers}
-        imageSrc={`${basePath}/img/maps/transylvania.jpeg`}
-        genericMarkers={genericMarkers}
-        labels={mapLabels}
-        markerSizeAnimal={settings.animalMarkerSize}
-        markerSizeGeneric={settings.genericMarkerSize}
-        markerSizeZone={settings.zoneMarkerSize}
-        markerTrophyRating={settings.animalMarkerRatings}
-        zoomMarkerMap={markerVisibilityMap}
-      />
-
-      {createPortal(tutorial, document.body)}
-    </>
-  );
-};
+const TransylvaniaPage = () => (
+  <HuntingMapPage
+    animalMarkers={animalMarkers}
+    genericMarkers={genericMarkers}
+    mapType="transylvania"
+    mapImageSrc="/img/maps/transylvania.jpeg"
+    mapLabels={mapLabels}
+    titleKey="POI:MAP_NAME_TRANSYLVANIA"
+  />
+);
 
 export default TransylvaniaPage;
