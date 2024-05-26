4dd68356019a28b1d6e8f32f944b8892b4ca2ea1
Codeaid <codeaid@users.noreply.github.com>
2024-05-26 21:04:08 +0100

    Removed legacy companion application scripts

--- src/scripts/animals.ts
+++
@@ -1,5 +0,0 @@
-import * as process from 'process';
-import { fauna } from 'config/animals';
-
-const json = JSON.stringify(fauna);
-process.stdout.write(json);
--- src/scripts/weapons.ts
+++
@@ -1,5 +0,0 @@
-import * as process from 'process';
-import { weapons } from 'config/weapons';
-
-const json = JSON.stringify(weapons);
-process.stdout.write(json);
