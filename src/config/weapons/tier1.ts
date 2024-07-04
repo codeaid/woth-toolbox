1034c568136ce77fb8f0b5299a8aef4ce23bf65a
Codeaid <codeaid@users.noreply.github.com>
2024-07-04 01:46:14 +0100

    Added Remington Firearms pack weapons

--- src/config/weapons/tier1.ts
+++ src/config/weapons/tier1.ts
@@ -38,6 +38,24 @@ export default [
     tier: 1,
   },
   {
+    action: 'WEAPON:SHOTGUN_PUMP_01_FIELD_01_ACTION',
+    caliber: 'WEAPON:SHOTGUN_PUMP_01_FIELD_01_CALIBER',
+    description: 'WEAPON:SHOTGUN_PUMP_01_FIELD_01_DESCRIPTION',
+    heading: 'WEAPON:SHOTGUN_PUMP_01_FIELD_01_HEADING',
+    hitEnergy: [0, 0, 0, 0, 0],
+    slug: 'remington-870-fieldmaster',
+    tier: 1,
+  },
+  {
+    action: 'WEAPON:SHOTGUN_PUMP_01_SPS_01_ACTION',
+    caliber: 'WEAPON:SHOTGUN_PUMP_01_SPS_01_CALIBER',
+    description: 'WEAPON:SHOTGUN_PUMP_01_SPS_01_DESCRIPTION',
+    heading: 'WEAPON:SHOTGUN_PUMP_01_SPS_01_HEADING',
+    hitEnergy: [0, 0, 0, 0, 0],
+    slug: 'remington-870-sps-super-magnum',
+    tier: 1,
+  },
+  {
     action: 'WEAPON:SHOTGUN_PUMP_01_ACTION',
     caliber: 'WEAPON:SHOTGUN_PUMP_01_CALIBER',
     description: 'WEAPON:SHOTGUN_PUMP_01_DESCRIPTION',
@@ -52,7 +70,7 @@ export default [
     description: 'WEAPON:SHOTGUN_SEMI_02_DESCRIPTION',
     heading: 'WEAPON:SHOTGUN_SEMI_02_HEADING',
     hitEnergy: [0, 0, 0, 0, 0],
-    slug: 'remington-v3-Waterfowl-pro',
+    slug: 'remington-v3-waterfowl-pro',
     tier: 1,
   },
 ] as Array<Weapon>;
