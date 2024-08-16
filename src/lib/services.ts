9ec51147a7c47bd4be9d7e854110a7ab0788575a
Codeaid <codeaid@users.noreply.github.com>
2024-08-16 21:53:53 +0100

    Removed Firebase GA measurement configuration

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -17,7 +17,6 @@ const createApp = () =>
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
