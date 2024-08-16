5ccd35962471d4fcec85d053b5db6215e2940688
Codeaid <codeaid@users.noreply.github.com>
2024-08-16 20:32:15 +0100

    Configured Firebase environment variable injection

--- src/lib/services.ts
+++ src/lib/services.ts
@@ -14,8 +14,13 @@ import {
  */
 const createApp = () =>
   initializeApp({
-    apiKey: 'toolbox',
-    projectId: 'demo-woth-toolbox',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
 
 /**
