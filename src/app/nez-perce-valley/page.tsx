6abe89dbd41d105085e9783d6f283330f06cab56
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-10-26 21:42:20 +0000

    Add conditional HerdMapOverlay to Nez Perce Valley page

--- src/app/nez-perce-valley/page.tsx
+++ src/app/nez-perce-valley/page.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import { HuntingMapPage } from 'components/HuntingMapPage';
+import { HerdMapOverlay } from 'components/HerdMapOverlay';
+import { isUserLoggedIn } from 'services/discordApiService';
 import { animalMarkers, genericMarkers, mapLabels } from 'config/idaho';
 
 const NezPerceValleyPage = () => (
-  <HuntingMapPage
-    animalMarkers={animalMarkers}
-    genericMarkers={genericMarkers}
-    mapId="idaho"
-    mapImageSrc="/img/maps/nez_perce.jpeg"
-    mapLabels={mapLabels}
-    titleKey="POI:MAP_NAME_IDAHO"
-  />
+  <>
+    <HuntingMapPage
+      animalMarkers={animalMarkers}
+      genericMarkers={genericMarkers}
+      mapId="idaho"
+      mapImageSrc="/img/maps/nez_perce.jpeg"
+      mapLabels={mapLabels}
+      titleKey="POI:MAP_NAME_IDAHO"
+    />
+    {isUserLoggedIn() && <HerdMapOverlay currentMap="Nez Perce Valley" />}
+  </>
 );
 
 export default NezPerceValleyPage;
