e20cada92b8d7c27de649710588c3eaedee24da4
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-10-27 19:13:48 +0000

    Refactor NezPerceValleyPage to use state for login

--- src/app/nez-perce-valley/page.tsx
+++ src/app/nez-perce-valley/page.tsx
@@ -1,21 +1,32 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { HerdMapOverlay } from 'components/HerdMapOverlay';
 import { HuntingMapPage } from 'components/HuntingMapPage';
 import { animalMarkers, genericMarkers, mapLabels } from 'config/idaho';
 import { isUserLoggedIn } from 'services/discordApiService';
-const NezPerceValleyPage = () => (
-  <>
-    <HuntingMapPage
-      animalMarkers={animalMarkers}
-      genericMarkers={genericMarkers}
-      mapId="idaho"
-      mapImageSrc="/img/maps/nez_perce.jpeg"
-      mapLabels={mapLabels}
-      titleKey="POI:MAP_NAME_IDAHO"
-    />
-    {isUserLoggedIn() && <HerdMapOverlay currentMap="Nez Perce Valley" />}
-  </>
-);
+
+const NezPerceValleyPage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Only check login status on client side
+    setIsLoggedIn(isUserLoggedIn());
+  }, []);
+
+  return (
+    <>
+      <HuntingMapPage
+        animalMarkers={animalMarkers}
+        genericMarkers={genericMarkers}
+        mapId="idaho"
+        mapImageSrc="/img/maps/nez_perce.jpeg"
+        mapLabels={mapLabels}
+        titleKey="POI:MAP_NAME_IDAHO"
+      />
+      {isLoggedIn && <HerdMapOverlay currentMap="Nez Perce Valley" />}
+    </>
+  );
+};
 
 export default NezPerceValleyPage;
