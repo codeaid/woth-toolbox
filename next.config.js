a71d987b78b5128d496c97701fa836adc416c6d3
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-10-27 22:12:48 +0000

    Remove unoptimized images configuration

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  images: {
-    unoptimized: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
