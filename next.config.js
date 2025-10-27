ae1b874ad142c16a159e5b2ee663f55f269a267b
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-10-27 22:23:40 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '',
-  images: {
-    unoptimized: true,
-  },
+  basePath: '/woth-toolbox',
   eslint: {
     ignoreDuringBuilds: true,
   },
