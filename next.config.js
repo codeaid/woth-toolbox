400d4c026909960a800e7a8ee0a80fb2ff092dd6
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-10-27 20:43:58 +0000

    Refactor next.config.js for clarity and updates
    Refactor Next.js configuration to simplify settings and remove experimental images.

--- next.config.js
+++ next.config.js
@@ -1,69 +1,26 @@
-const path = require('path');
-const loaderUtils = require('loader-utils');
-
-// @see https://gist.github.com/herlon214/7da6a39b885014a5d5f51716f6530ce4
-const getLocalIdentHash = (context, localIdentName, localName) =>
-  loaderUtils
-    .getHashDigest(
-      Buffer.from(
-        `filePath:${path
-          .relative(context.rootContext, context.resourcePath)
-          .replace(/\\+/g, '/')}#className:${localName}`,
-      ),
-      'md4',
-      'base64',
-      6,
-    )
-    .replace(/^(-?\d|--)/, '_$1')
-    .replaceAll('+', '_')
-    .replaceAll('/', '_');
-
 /** @type {import('next').NextConfig} */
-const config = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
+// Clean up experimental.images if it exists
+const nextConfig = {
+  basePath: '/woth-toolbox',
+  assetPrefix: '/woth-toolbox/',
   output: 'export',
-  images: { unoptimized: true },
-  reactStrictMode: true,
-  trailingSlash: false,
-  webpack: (config, { dev }) => {
-    const rules = config.module.rules
-      .find(rule => typeof rule.oneOf === 'object')
-      .oneOf.filter(rule => Array.isArray(rule.use));
-
-    // Enable importing SVG images as components
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      test: /\.md$/,
+      use: 'raw-loader',
     });
-
-    // Don't change names in development mode
-    if (dev) {
-      return config;
-    }
-
-    rules.forEach(rule => {
-      rule.use.forEach(moduleLoader => {
-        if (
-          moduleLoader.loader?.includes('css-loader') &&
-          !moduleLoader.loader?.includes('postcss-loader')
-        )
-          moduleLoader.options.getLocalIdent = getLocalIdentHash;
-      });
-    });
-
     return config;
-  }
+  },
 };
 
-// Remove unsupported experimental.images for Next.js 15
-if (config.experimental?.images) {
-  delete config.experimental.images;
-  if (Object.keys(config.experimental).length === 0) {
-    delete config.experimental;
-  }
+// Remove experimental.images if it exists to avoid Next.js 15 error
+if (nextConfig.experimental?.images) {
+  delete nextConfig.experimental.images;
 }
 
-module.exports = config;
+module.exports = nextConfig;
