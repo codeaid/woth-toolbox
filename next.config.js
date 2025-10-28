2390d60f7d9deebf1cb2fa61549e821749872186
smithyhelen <smithandsonsdecorating.as@gmail.com>
2025-10-28 13:25:59 +0000

    Restore original next.config.js with webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,62 @@
-const nextConfig = {
-  output: 'export',
-  basePath: '/woth-toolbox',
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+// @see https://gist.github.com/herlon214/7da6a39b885014a5d5f51716f6530ce4
+const getLocalIdentHash = (context, localIdentName, localName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, '/')}#className:${localName}`,
+      ),
+      'md4',
+      'base64',
+      6,
+    )
+    .replace(/^(-?\d|--)/, '_$1')
+    .replaceAll('+', '_')
+    .replaceAll('/', '_');
+
+/** @type {import('next').NextConfig} */
+const config = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+
+  // Don't run linter during production builds
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  reactStrictMode: true,
+  trailingSlash: false,
+  webpack: (config, { dev }) => {
+    const rules = config.module.rules
+      .find(rule => typeof rule.oneOf === 'object')
+      .oneOf.filter(rule => Array.isArray(rule.use));
+
+    // Enable importing SVG images as components
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    // Don't change names in development mode
+    if (dev) {
+      return config;
+    }
+
+    rules.forEach(rule => {
+      rule.use.forEach(moduleLoader => {
+        if (
+          moduleLoader.loader?.includes('css-loader') &&
+          !moduleLoader.loader?.includes('postcss-loader')
+        )
+          moduleLoader.options.getLocalIdent = getLocalIdentHash;
+      });
+    });
+
+    return config;
+  }
 };
 
-module.exports = nextConfig;
+module.exports = config;
